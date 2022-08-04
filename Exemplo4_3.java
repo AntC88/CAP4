139b89f47c3f23b421a1722bab1cd9ed93bd6883
AntonioJJC <antoniojjcintra1@gmail.com>
2022-08-04 16:29:43 -0300

    1.1

--- Exemplo4_3.java
+++ Exemplo4_3.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class Exemplo4_3 {
   public static void main(String[] args) {
     String nome;
-    nome = JOptionPane.showInputDialog("Qual o seu Nom?");
+    nome = JOptionPane.showInputDialog("Qual o seu Nome?");
     JOptionPane.showMessageDialog(null, "Seu nome �: " + nome);
   }
 }
